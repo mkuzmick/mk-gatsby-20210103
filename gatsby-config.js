8993444cd2d3e58bd2349d68f35627ee866f7e40
Marlon Kuzmick <mkuzmick@icloud.com>
2021-01-03 07:25:47 -0500

    Updates: initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `MK Gatsby Project for 20210103`,
+    description: `Another daily Gatsby project. This one from the vanilla starter.`,
+    author: `Marlon Kuzmick`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
