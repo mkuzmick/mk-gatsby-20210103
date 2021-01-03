f169e766fdf4ed285f9f19b33a37d688c24e4a60
Marlon Kuzmick <mkuzmick@icloud.com>
2021-01-03 17:21:25 -0500

    Updates: trying other audiocontext solution

--- src/components/piano/simple-keyboard.js
+++ src/components/piano/simple-keyboard.js
@@ -1,95 +1,52 @@
-import React from 'react';
+import React from 'react'
 import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
 import 'react-piano/dist/styles.css';
 
-import DimensionsProvider from './DimensionsProvider';
 import SoundfontProvider from './SoundfontProvider';
 import './styles.css';
 
-// webkitAudioContext fallback needed to support Safari
+
+
 const soundfontHostname = 'https://d1pzp51pvbm36p.cloudfront.net';
 
 const noteRange = {
   first: MidiNumbers.fromNote('c3'),
-  last: MidiNumbers.fromNote('f4'),
+  last: MidiNumbers.fromNote('c6'),
 };
+
 const keyboardShortcuts = KeyboardShortcuts.create({
   firstNote: noteRange.first,
   lastNote: noteRange.last,
   keyboardConfig: KeyboardShortcuts.HOME_ROW,
 });
 
-function SimpleKeyboard() {
+
+const SimpleKeyboard = (props) => {
+  let audioContext
+  if (typeof window !== `undefined`) {
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  }
   return (
     <div>
-      <h1>react-piano demos</h1>
-      <div className="mt-5">
-        <p>Basic piano with hardcoded width</p>
-        <BasicPiano />
-      </div>
-
-      <div className="mt-5">
-        <p>
-          Responsive piano which resizes to container's width. Try resizing the
-          window!
-        </p>
-        <ResponsivePiano />
-      </div>
-
-      <div className="mt-5">
-        <p>Piano with custom styling - see styles.css</p>
-        <ResponsivePiano className="PianoDarkTheme" />
-      </div>
+      <SoundfontProvider
+    instrumentName="acoustic_grand_piano"
+    audioContext={audioContext}
+    hostname={soundfontHostname}
+    render={({ isLoading, playNote, stopNote }) => (
+      <Piano
+        noteRange={noteRange}
+        width={600}
+        playNote={playNote}
+        stopNote={stopNote}
+        disabled={isLoading}
+        keyboardShortcuts={keyboardShortcuts}
+      />
+    )}
+  />
     </div>
   );
 }
 
-function BasicPiano() {
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-  return (
-    <SoundfontProvider
-      instrumentName="acoustic_grand_piano"
-      audioContext={audioContext}
-      hostname={soundfontHostname}
-      render={({ isLoading, playNote, stopNote }) => (
-        <Piano
-          noteRange={noteRange}
-          width={300}
-          playNote={playNote}
-          stopNote={stopNote}
-          disabled={isLoading}
-          keyboardShortcuts={keyboardShortcuts}
-        />
-      )}
-    />
-  );
-}
-
-function ResponsivePiano(props) {
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-  return (
-    <DimensionsProvider>
-      {({ containerWidth, containerHeight }) => (
-        <SoundfontProvider
-          instrumentName="acoustic_grand_piano"
-          audioContext={audioContext}
-          hostname={soundfontHostname}
-          render={({ isLoading, playNote, stopNote }) => (
-            <Piano
-              noteRange={noteRange}
-              width={containerWidth}
-              playNote={playNote}
-              stopNote={stopNote}
-              disabled={isLoading}
-              {...props}
-            />
-          )}
-        />
-      )}
-    </DimensionsProvider>
-  );
-}
-
 export default SimpleKeyboard
+
+
